8775071aeba9eed9aedaebed4df5389a9c539429
Ever-Miller <3verjmiller@gmail.com>
2024-10-28 18:28:13 -0700

    Added helper functions for clarity

--- MindGarden_V2/models.py
+++ MindGarden_V2/models.py
@@ -8,6 +8,7 @@ class LinkedListQueue:
     def __init__(self):
         self._head = None
         self._tail = None
+        self._length = 0
 
     def queue(self, data):
         node = Node(data)
@@ -18,6 +19,7 @@ class LinkedListQueue:
             self._head._prev = node
             node._next = self._head
             self._head = node
+        self._length += 1
 
     def dequeue(self):
         if not self._head:
@@ -26,27 +28,19 @@ class LinkedListQueue:
             node = self._head 
             self._head = None
             self._tail = None
-            return node._data
         else:
             node = self._tail
             self._tail = node._prev
             self._tail._next = None
             node._prev = None 
-            return node._data
+        self._length -= 1
+        return node._data
 
     def is_empty(self):
         return self._head == None
 
     def __len__(self):
-        if not self._head:
-            return 0
-        else:
-            i = 0
-            cur = self._head
-            while cur != None:
-                i += 1
-                cur = cur._next
-            return i
+        return self._length
 
     def __repr__(self):
         return self.__str__()
@@ -100,12 +94,6 @@ class UserStats:
     def add_deck_stats(self, deck):
         self._deck_stats[deck.get_name()] = deck.stats()
 
-class DeckStats:
-    def __init__(self):
-        self._date_added = time.time()
-        self._total_reviews = 0
-        self._time_studied = 0
-        self._quality_rate = 0
 
 class CardStats:
     def __init__(self, card):
@@ -139,7 +127,7 @@ class CardStats:
             elif self._review_count == 1:
                 self._interval = 3
             else:
-                self.interval = round(self.interval * self._ease)
+                self._interval = round(self._interval * self._ease)
         # Update ease factor
         self._ease += (0.1 - (difficulty - 1) * (0.08 + (difficulty - 1) * 0.02))
         self._ease = max(0.3, self._ease)  
@@ -239,69 +227,125 @@ class Card():
     def __str__(self):
         return f"( Front: {self._front} | Back: {self._back} )"
 
+class DeckStats:
+    def __init__(self):
+        self._date_added = datetime.now().isoformat().split("T")[0]
+        self._card_count = 0
+        self._total_reviews = 0
+        self._time_studied = 0
+        self._difficulty_sum = 0
+        self._avg_difficulty = 0
+        self._max_new = 3
+        self._deck_ease = 1
+    
+    def incr_card_count(self):
+        self._card_count += 1
 
-class Deck(DeckStats):
+    def get_max_new(self):
+        return self._max_new
+
+    def update_stats(self, elapsed_time, difficulty_sum, review_count):
+        self._total_reviews += review_count
+        self._time_studied += round((elapsed_time / 1440), 3)
+        self._difficulty_sum += difficulty_sum
+        self._avg_difficulty = round((self._difficulty_sum / self._total_reviews), 1)
+    
+    def get_stats(self):
+        stats = {
+            "Total Cards": self._card_count,
+            "Date Added": self._date_added,
+            "Total Reviews": self._total_reviews,
+            "Time Studied (h)": self._time_studied,
+            "Average Difficulty": self._avg_difficulty if self._total_reviews > 0 else "N/A",
+            "Deck Ease": self._deck_ease
+        }
+        return stats
+
+
+class Deck():
     def __init__(self, name):
         self._name = name
         self._study_deck = StudyDeck()
         self._new_cards_deck = NewCardsDeck()
-        self._max_new = 3
         self._stats = DeckStats()
 
     def add_card(self, card):
+        self._stats.incr_card_count()
         self._new_cards_deck.queue_card(card)
         if card.get_reversed_card():
             self._new_cards_deck.queue_card(card.get_reversed_card())
+            self._stats.incr_card_count()
 
     def study(self):
         review_queue = self.create_review_queue()
         session_start = time.time()
-        print(session_start)
+
         review_count = 0
+        difficulty_sum = 0
+
         while not review_queue.is_empty():
             cur_card = review_queue.dequeue()
-            start_time = time.time()
-            print("+---------------------------------------------------------+")
-            print("                                                          |")
-            print(f"Card Front:\n\n{cur_card.get_front()}\n")
-            print("                                                          |")
-            print("+---------------------------------------------------------+")
+            if not self.display_card(cur_card):
+                break  # User chose to end the session
 
-            user_input = input()
-            if user_input == "":
-                print("+---------------------------------------------------------+")
-                print("                                                          |")
-                print(f"Card Back\n\n{cur_card.get_back()}\n")
-                print("                                                          |")
-                print("+---------------------------------------------------------+")
-            elif user_input == "BREAK":
-                break
-            
-            difficulty = int(input("How hard was that card? (1-5): "))
-            if difficulty >= 4:
-                review_queue.queue(cur_card)
-            else:
-                end_time = time.time()
-                cur_card.update_stats(start_time, end_time, difficulty)
-                review_count += 1
-                self._study_deck.add_card(cur_card.get_next_due(), cur_card)
-        
+            difficulty = self.review_card(cur_card)
+            difficulty_sum += difficulty
+            review_count += 1
+
         session_end = time.time()
+        elapsed_time = session_end - session_start
+        self.update_stats(elapsed_time, difficulty_sum, review_count)
         print("You are done reviewing this deck for today!")
     
+    def display_card(self, card):
+        """Displays card front and back based on user input."""
+        print("+---------------------------------------------------------+")
+        print("                                                          |")
+        print(f"Card Front:\n\n{card.get_front()}\n")
+        print("                                                          |")
+        print("+---------------------------------------------------------+")
+        user_input = input("Press Enter to view the back or 'BREAK' to end: ")
+        
+        if user_input == "":
+            print("+---------------------------------------------------------+")
+            print("                                                          |")
+            print(f"Card Back\n\n{card.get_back()}\n")
+            print("                                                          |")
+            print("+---------------------------------------------------------+")
+        elif user_input.upper() == "BREAK":
+            return False  # Break out of study loop
+        return True  # Continue studying
+
+    def review_card(self, card):
+        """Processes a single card review, updates stats based on difficulty."""
+        start_time = time.time()
+        difficulty = int(input("How hard was that card? (1-5): "))
+        end_time = time.time()
+
+        if difficulty >= 4:
+            self.review_queue.queue(card)  # Queue for re-review
+        else:
+            card.update_stats(start_time, end_time, difficulty)
+            self._study_deck.add_card(card.get_next_due(), card)
+
+        return difficulty
+
+
     def create_review_queue(self):
         date = datetime.now().isoformat().split("T")[0]
+
+        max_new = self._stats.get_max_new()
         
         if date in self._study_deck:
             study_queue = self._study_deck.get_deck()[date]
         else:
             study_queue = LinkedListQueue()
 
-        ratio = max(len(study_queue) // (min(self._max_new, len(self._new_cards_deck.get_deck()))), 1)
+        ratio = max(len(study_queue) // (min(max_new, len(self._new_cards_deck.get_deck()))), 1)
 
 
         cur_review_queue = LinkedListQueue()
-        for _ in range(self._max_new):
+        for _ in range(max_new):
             if self._new_cards_deck.get_deck().is_empty():
                 break
             else:
@@ -316,9 +360,11 @@ class Deck(DeckStats):
 
         return cur_review_queue
 
+    def update_stats(self, elapsed_time, difficulty_sum, review_count):
+        self._stats.update_stats(elapsed_time, difficulty_sum, review_count)
 
     def get_stats(self):
-        return self._stats
+        return self._stats.get_stats()
 
     def get_name(self):
         return self._name
@@ -402,6 +448,7 @@ def main():
     h = input()
     print(card.get_stats())
     print(deck.get_study_deck())
+    print(deck.get_stats())
     
     
 
